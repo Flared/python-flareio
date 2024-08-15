47dbaececbcd1a3d72526145014f06ca34e6f819
aviau <alexandre@alexandreviau.net>
2024-08-15 14:20:06 -0400

    restore test_generate_token

--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -1,6 +1,7 @@
 import pytest
 import requests_mock
 
+from datetime import datetime
 from flareio import FlareApiClient
 from flareio.exceptions import TokenError
 
@@ -21,6 +22,32 @@ def test_create_client_empty_api_key() -> None:
         FlareApiClient(
             api_key="",
         )
+
+
+def test_generate_token() -> None:
+    client = _get_test_client()
+    assert client.token is None
+    assert client.token_exp is None
+    with requests_mock.Mocker() as mocker:
+        mocker.register_uri(
+            "POST",
+            "https://api.flare.io/tokens/generate",
+            json={
+                "token": "test-token-hello",
+            },
+            status_code=200,
+        )
+
+        token = client.generate_token()
+        assert token == "test-token-hello"
+
+        assert client.token == "test-token-hello"
+        assert client.token_exp
+        assert client.token_exp >= datetime.now()
+
+        assert mocker.last_request.url == "https://api.flare.io/tokens/generate"
+        assert mocker.last_request.text is None
+        assert mocker.last_request.headers["Authorization"] == "test-api-key"
 
 
 def test_generate_token_error() -> None:
